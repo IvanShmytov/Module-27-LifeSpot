fd433a799911999b6587549a145da85b362bb170
IvanShmytov <100521162+IvanShmytov@users.noreply.github.com>
2022-10-01 14:16:28 +0400

    Add files via upload
    Выполнены задания модулей 28-29 (JavaScript).

---
+++ LifeSpot/Static/JS/about.js
@@ -0,0 +1,24 @@
+﻿function getReview() {
+    let review = new Map();
+
+    review.set("userName", prompt("Как вас зовут ?"));
+    if (review.get("userName") == null) {
+        return;
+    }
+
+    review.set("comment", prompt("Напишите свой отзыв"));
+    if (review.get("comment") == null) {
+        return
+    }
+
+    review.set("date", new Date().toLocaleString());
+
+    writeReview(review)
+}
+
+const writeReview = review => {
+    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+        `<p> <i> <b>${review.get("userName")}</b>  ${review.get("comment")}</i></p>` +
+        `<p>${review.get("date")}</p>` +
+        '</div>';
+}
---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,48 @@
+﻿let session = new Map();
+
+setTimeout(function () {
+    alert("Нравится LifeSpot? " + '\n' + "Подпишитесь на наш Instagram @lifespot999!");
+}, 60000);
+
+const inputParseFunction = function () {
+    return document.getElementsByTagName('input')[0].value.toLowerCase();
+}
+function handleSession() {
+    session.set("startDate", new Date().toLocaleString())
+    session.set("userAgent", window.navigator.userAgent)
+}
+function checkAge(){
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+    
+    if(session.get("age") >= 18){
+        alert("Приветствуем на LifeSpot! " + '\n' +  "Текущее время: " + new Date().toLocaleString() );
+    }
+    else{
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+
+let sessionLog = function logSession()
+{
+    for (let item of session)
+    {
+        console.log(item)
+    }
+}
+
+function filterContent() {
+    let elements = document.getElementsByClassName('video-container');
+    for (let i = 0; i <= elements.length; i++) {
+        let childElements = elements[i];
+        let videoDescription = childElements.getElementsByTagName('h3')[0].innerText;
+        if (!videoDescription.toLowerCase().includes(inputParseFunction().toLowerCase()))
+        {
+            elements[i].style.display = 'none';
+        }
+        else
+        {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
