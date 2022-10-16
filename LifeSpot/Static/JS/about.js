b5b32cd205b36a7eb0f80e1730236545a3022aa2
IvanShmytov <100521162+IvanShmytov@users.noreply.github.com>
2022-10-16 16:44:40 +0400

    Add files via upload
    Выполнено итоговое задание модуля 31 (31.4. СДАЧА ПРОЕКТА LIFESPOT (HW-03))

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -1,24 +1,85 @@
-﻿function getReview() {
-    let review = new Map();
-
-    review.set("userName", prompt("Как вас зовут ?"));
-    if (review.get("userName") == null) {
-        return;
-    }
-
-    review.set("comment", prompt("Напишите свой отзыв"));
-    if (review.get("comment") == null) {
+﻿function Comment() {
+    this.author = prompt("Как вас зовут ?")
+    if (this.author == null) {
+        this.empty = true
         return
     }
 
-    review.set("date", new Date().toLocaleString());
+    this.text = prompt("Оставьте отзыв")
+    if (this.text == null) {
+        this.empty = true
+        return
+    }
 
-    writeReview(review)
+    this.date = new Date().toLocaleString()
+}
+
+function addComment() {
+    let comment = new Comment()
+
+    if (comment.empty)
+    {
+        return;
+    }
+    
+    let enableLikes = confirm('Разрешить пользователям оценивать ваш отзыв?')
+
+    if (enableLikes) {
+        // Создадим для отзыва новый объект из прототипа - комментария
+        let review = Object.create(comment)
+        // и добавим ему нужное свойство
+        review.rate = 0;
+
+        // Добавляем отзыв с возможностью пользовательских оценок
+        writeReview(review)
+    } else {
+        // Добавим простой комментарий без возможности оценки
+        writeReview(comment)
+    }
 }
 
 const writeReview = review => {
+    let likeCounter = '';
+
+    // Для проверки, является ли объект отзывом, используем свойство hasOwnProperty
+    if (review.hasOwnProperty('rate')) {
+        let commentId = Math.random();
+        likeCounter += '<button id="' + commentId + '" style="border: none" onclick="addLike(this.id)">' + `❤️ ${review.rate}</button>`
+    }
+
+    // Запишем результат
     document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
-        `<p> <i> <b>${review.get("userName")}</b>  ${review.get("comment")}</i></p>` +
-        `<p>${review.get("date")}</p>` +
+        `<p> <i> <b>${review['author']}</b>  ${review['date']}${likeCounter}</i></p>` +
+        `<p>${review['text']}</p>` +
         '</div>';
+}
+function addLike(id) {
+    let element = document.getElementById(id);
+
+    let array = element.innerText.split(' ')
+
+ 
+    let resultNum = parseInt(array[array.length - 1], 10);
+
+    resultNum += 1
+
+    array[array.length - 1] = `${resultNum}`
+
+    element.innerText = array.join(' ')
+}
+
+function switchImage() {
+    let images = document.getElementsByTagName('img');
+    for (let i = 0; i < images.length; i++) {
+        if (images[i].style.display != 'none') {
+            if (i != images.length - 1) {
+                images[i + 1].style.display = 'inline-block';
+            }
+            else {
+                images[0].style.display = 'inline-block';
+            }
+            images[i].style.display = 'none';           
+            break;
+        }
+    }
 }
